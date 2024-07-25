250967bf555f7f3a02df6272f7c6b6385e1b1f61
Macanegreen <78729006+Macanegreen@users.noreply.github.com>
2024-07-25 19:42:14 +0200

    Some lazyloading and better navbar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    var lazyloadImages;
+
+//    if ("IntersectionObserver" in window) {
+//        lazyloadImages = document.querySelectorAll(".lazy");
+//        var imageObserver = new IntersectionObserver(function (entries, observer) {
+//            entries.forEach(function (entry) {
+//                if (entry.isIntersecting) {
+//                    var image = entry.target;
+//                    image.src = image.dataset.src;
+//                    image.classList.remove("lazy");
+//                    imageObserver.unobserve(image);
+//                }
+//            });
+//        }, {
+//            root: document.querySelector("#img-container"),
+//            rootMargin: "0px 0px 800px 0px"
+//        });
+
+//        lazyloadImages.forEach(function (image) {
+//            imageObserver.observe(image);
+//        });
+//    } else {
+        
+//    }
+//})
